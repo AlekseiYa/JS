33f5c230220cf15ed89e76e839e91f76ef63ff52
AlekseiYa <Aleksei_yanshin@icloud.com>
2024-01-09 15:50:04 +0300

    Домашнее задание по  JS #1

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
-let n = 1
+/*// Задание первое:
+let n = prompt('Введите число:')
+let fun = n**2
+console.log(`"ответ: ", ${fun}`)*/
 
-console .log(typeof n)
+/*// Задание второе:
+let num = prompt("Введите первое число: ");
+let num2 = prompt("Введите второе число:");
+let sum = (num + num2)
+console.log(sum / 2)*/
+
+/*// Задача три:
+let line = prompt('Введите длину квадрата: ')
+let s = line*line
+console.log(`Площадь квадрата со стороной ${line} = ${s}`)*/
+
+/* Задача четыре:
+// const km = prompt("Колл-во километров:")
+// const fact = 0.621371
+// const miles = km * fact
+// console.log(`${km} км равны ${miles} миль`)*/
+
+/* задача пять
+let n1 = Number(prompt('enter number one'))
+let n2 = Number(prompt('enter number two '))
+let op = prompt('enter operation')
+
+switch(op){
+    case '+': alert((n1 + n2));
+    break;
+    case '-': alert((n1 - n2));
+    break;
+    case '*': alert((n1 * n2));
+    break;
+    case '/': alert((n1 / n2));
+    break;
+}*/
+
+/*Задание шесть
+let a = prompt('enter meaning one');
+let b = prompt('enter meaning two');
+
+let x = a * b;
+alert('Значение x: ' + x);*/
+
+/*Задание семь
+let hours = +prompt("Введите часы (0 - 23): ")
+let minutes = +prompt('введите минуты (0 - 59): ')
+
+let reHours = 23 - hours
+let reMinutes = 60 - minutes
+
+console.log('Оствшиеся часы: ', reHours)
+console.log('Оставшиеся минут:', reMinutes)*/
+
+
+
+
+
+
+
+
+
